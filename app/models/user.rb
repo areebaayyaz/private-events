de898567708c0113061aedfb6fbd678182759f1f
areebaayyaz <areebaayyaz@gmail.com>
2019-07-14 08:35:40 -0700

    Add association between event attendee (User) and Event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
 	validates :name, presence: true, uniqueness: { case_sensitive: false }
 	has_many :created_events, dependent: :destroy, foreign_key: "creator_id", class_name: "Event"
+  has_many :event_attendings, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendings
 end
