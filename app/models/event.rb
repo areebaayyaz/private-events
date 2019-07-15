0abd2d3ccfc3501d8eac4db4b0771d3dc3498d86
areebaayyaz <areebaayyaz@gmail.com>
2019-07-15 00:05:43 -0700

    modify User's show page to separate upcoming and previously attended events, using scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
 	belongs_to :creator, class_name: "User"
 	has_many :event_attendings, foreign_key: :attended_event_id
 	has_many :attendees, through: :event_attendings, source: :attendee
+
+  scope :upcoming, lambda { where('event_date >= ?', Date.current) }
+  scope :past, lambda { where('event_date < ?', Date.current) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get    '/login',   to: 'sessions#new'
+	root to:'users#index'
+  get    'login',   to: 'sessions#new'
   post   'login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 	resources :users
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
