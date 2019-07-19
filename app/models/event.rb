7cd82713d4b2b3ecf55df92e143b6ad73391b2db
areebaayyaz <areebaayyaz@gmail.com>
2019-07-19 02:28:41 -0700

    add dependency on deletion in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :event_attendings, foreign_key: :attended_event_id
-  has_many :attendees, through: :event_attendings, source: :attendee
+  has_many :event_attendings, foreign_key: :attended_event_id, dependent: :delete_all
+  has_many :attendees, through: :event_attendings, source: :attendee, dependent: :delete_all
 
   scope :upcoming, -> { where('event_date >= ?', Date.current) }
   scope :past, -> { where('event_date < ?', Date.current) }
